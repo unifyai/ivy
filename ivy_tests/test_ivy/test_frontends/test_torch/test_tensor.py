0fc9ec4cfe348f877263752125ba9fdcfbcb5f65
Jackson McClintock <jacksondm33@gmail.com>
2024-02-09 22:02:03 -0500

    fix: frontend test errors/inconsistencies for torch.Tensor.__lt__

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -878,11 +878,8 @@ def test_torch___long__(
     init_tree="torch.tensor",
     method_name="__lt__",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
+        available_dtypes=helpers.get_dtypes("valid"),
         num_arrays=2,
-        min_value=-1e04,
-        max_value=1e04,
-        allow_inf=False,
     ),
 )
 def test_torch___lt__(
@@ -896,12 +893,12 @@ def test_torch___lt__(
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
+        init_input_dtypes=[input_dtype[0]],
         backend_to_test=backend_fw,
         init_all_as_kwargs_np={
             "data": x[0],
         },
-        method_input_dtypes=input_dtype,
+        method_input_dtypes=[input_dtype[1]],
         method_all_as_kwargs_np={
             "other": x[1],
         },
